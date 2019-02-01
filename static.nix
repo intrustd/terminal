e8431b2a9425ef2a54b58aec3752b3db4bc0ded4
Travis Athougies <travis@athougies.net>
2019-01-31 22:05:11 -0800

    Add static assets

---
+++ static.nix
@@ -0,0 +1,25 @@
+{ pkgs, stdenv, manifest }:
+
+let nodePkgSet = import ./js { pkgs = pkgs.buildPackages; nodejs = pkgs.buildPackages."nodejs-8_x"; };
+
+    nodeDeps = (nodePkgSet.shell.override { bypassCache = true; }).nodeDependencies;
+
+in stdenv.mkDerivation {
+  name = "terminal-static";
+  src = ./js;
+
+  nativeBuildInputs = with pkgs; [ nodeDeps nodejs-8_x ];
+
+  phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+
+  buildPhase = ''
+    ln -s ${nodeDeps}/lib/node_modules ./node_modules
+    ln -s ${nodeDeps}/lib/package-lock.json ./package-lock.json
+    npm run build
+  '';
+
+  installPhase = ''
+     cp -R ./dist $out
+     cp ${manifest} $out/manifest.json
+  '';
+}

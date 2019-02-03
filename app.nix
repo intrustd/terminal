de75077475600f2bd4c948684692d994ff2fe8a4
Travis Athougies <travis@athougies.net>
2019-02-03 14:13:25 -0800

    Add new webpack configuration

--- app.nix
+++ app.nix
@@ -17,12 +17,77 @@ in {
 
   app.startHook = ''
     export PATH=/bin
-    exec ${ttyd}/bin/gotty ${pkgs.bash}/bin/bash
+    exec ${ttyd}/bin/ttyd -p 80 ${pkgs.bash}/bin/bash
   '';
 
   app.healthCheckHook = ''
     ${pkgs.procps}/bin/ps -A | ${pkgs.gnugrep}/bin/grep ttyd
     exit $?
   '';
+
+  app.permissions = [
+    { name = "access";
+      description = "Access a terminal on this appliance"; }
+  ];
+
+  app.permsHook =
+    let hook = pkgs.writeScript "terminal-perms" ''
+        #!/bin/sh
+        ACTION=none
+        PERSONA=
+        APP=
+
+        while [ $# -ne 0 ]; do
+          case "$1" in
+            -L|--lookup)   ACTION=lookup ;;
+            -c|--check)    ACTION=check ;;
+            -D|--describe) ACTION=describe ;;
+            -p|--persona)  PERSONA="$2"; shift ;;
+            -a|--application) APP="$2"; shift ;;
+          esac
+          shift
+        done
+
+        case "$ACTION" in
+          lookup)
+            echo "Do not understand -L or --lookup yet" >2
+            exit 2
+            ;;
+
+          check)
+            echo "Do not understand -c or --check yet" >2
+            exit 2
+            ;;
+
+          describe)
+            echo '['
+
+            while read PERM; do
+              case "$PERM" in
+                intrustd+perm://terminal.intrustd.com/access)
+                  cat <<EOF
+                   { "short": "Access a terminal via this appliance" }
+        EOF
+                  ;;
+
+                "")
+                  break
+                  ;;
+
+                *)
+                  ;;
+              esac
+            done
+
+            echo ']'
+            ;;
+
+          *)
+            echo "Invalid command"
+            exit 2
+            ;;
+        esac
+      '';
+    in "${hook}";
 
 }
